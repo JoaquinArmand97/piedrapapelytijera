becb6b041c1a87dd3651f6b9d7304156e76803e9
Joaquin Armand <djoaquinaa@gmail.com>
2024-07-01 18:58:52 -0300

    LocalStorge

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
     const juego = new Juego();
-    document.getElementById('jugarBtn').addEventListener('click', function() {
-        document.querySelector('.hidden').classList.add('inicio');
-        document.querySelector('.hidden').classList.remove('hidden');
+    const jugarBtn = document.getElementById('jugarBtn');
+    const jugarBtn2 = document.getElementById('jugarBtn2');
+    const nombreInput = document.getElementById('nombreInput');
+    const jugarBtn3 = document.getElementById('jugarBtn3');
+    const nombreJugador = document.getElementById('nombreJugador');
+
+    mostrarUltimosJugadores();
+
+    jugarBtn.addEventListener('click', function() {
+        nombreInput.classList.remove('hidden');
+        jugarBtn.style.display = 'none';
+    });
+
+    jugarBtn3.addEventListener('click', function() {
+        const nombre = nombreJugador.value.trim();
+        if (nombre) {
+            juego.setNombreJugador(nombre);
+            nombreInput.classList.add('hidden');
+            document.querySelector('.imagenes').classList.remove('escondido');
+            juego.inicioJuego();
+            guardarJugador(nombre);
+        } else {
+            alert("Por favor, ingresa un nombre.");
+        }
+    });
+
+    jugarBtn2.addEventListener('click', function() {
+        jugarBtn2.classList.add('escondido');
+        document.querySelector('.imagenes').classList.remove('escondido');
+        const mensajeFinal = document.querySelector('.mensajeFinal');
+        if (mensajeFinal) {
+            mensajeFinal.remove();
+        }
         juego.inicioJuego();
     });
+
     document.getElementById('piedra').addEventListener('click', function() {
         juego.usuarioElige('Piedra');
     });
@@ -14,6 +45,26 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('tijera').addEventListener('click', function() {
         juego.usuarioElige('Tijera');
     });
+
+    function guardarJugador(nombre) {
+        let jugadores = JSON.parse(localStorage.getItem('jugadores')) || [];
+        jugadores.unshift(nombre);
+        if (jugadores.length > 5) {
+            jugadores.pop();
+        }
+        localStorage.setItem('jugadores', JSON.stringify(jugadores));
+    }
+
+    function mostrarUltimosJugadores() {
+        let jugadores = JSON.parse(localStorage.getItem('jugadores')) || [];
+        const listaJugadores = document.getElementById('listaJugadores');
+        listaJugadores.innerHTML = '';
+        jugadores.forEach((jugador) => {
+            const li = document.createElement('li');
+            li.textContent = jugador;
+            listaJugadores.appendChild(li);
+        });
+    }
 });
 
 class Jugador {
@@ -29,67 +80,49 @@ class Jugador {
 
 class Juego {
     constructor() {
-        this.jugador = new Jugador('Jugador');
+        this.jugador = new Jugador('');
         this.pc = new Jugador('Computadora');
         this.jugadas = ["Piedra", "Papel", "Tijera"];
         console.log("Bienvenidos a Piedra, Papel o Tijera");
     }
 
+    setNombreJugador(nombre) {
+        this.jugador.nombre = nombre;
+        document.getElementById('victoriasJugador').innerText = `Victorias de ${nombre} : 0 `;
+    }
+
     inicioJuego() {
         this.jugador.victorias = 0;
         this.pc.victorias = 0;
-
-        while (this.jugador.victorias < 5 && this.pc.victorias < 5) {
-            let jugadaDelJugador = prompt("Elige una: " + this.jugadas.join(", ")).toLowerCase();
-            let jugadasMin = this.jugadas.map(jugada => jugada.toLowerCase());
-
-            if (!jugadasMin.includes(jugadaDelJugador)) {
-                console.log("Entrada no válida. Por favor elige Piedra, Papel o Tijera.");
-                continue;
-            }
-
-            let jugadorEleccion = this.jugadas[jugadasMin.indexOf(jugadaDelJugador)]; 
-            let indiceAleatorio = Math.floor(Math.random() * this.jugadas.length);
-            let pcEleccion = this.jugadas[indiceAleatorio];
-
-            console.log("El jugador ha elegido: " + jugadorEleccion);
-            console.log("La computadora ha elegido: " + pcEleccion);
-
-            let resultado = this.ganador(jugadorEleccion, pcEleccion); 
-
-            if (resultado === "¡Ganaste!") {
-                this.jugador.incrementarVictorias();
-            } else if (resultado === "¡Perdiste!") {
-                this.pc.incrementarVictorias();
-            }
-
-            console.log("Victorias del jugador: " + this.jugador.victorias);
-            console.log("Victorias de la computadora: " + this.pc.victorias);
+        this.actualizarVictorias();
+        document.querySelector('.imagenes').classList.remove('escondido');
+        document.getElementById('jugarBtn2').classList.add('escondido');
+        const mensajeFinal = document.querySelector('.mensajeFinal');
+        if (mensajeFinal) {
+            mensajeFinal.remove();
         }
-
-        if (this.jugador.victorias === 5) {
-            console.log("¡Felicidades! Has ganado la partida.");
-        } else if (this.pc.victorias === 5) {
-            console.log("La computadora ha ganado la partida. ¡Mejor suerte la próxima vez!");
-        }
-
-        console.log("Fin del juego. Gracias por jugar!");
     }
 
     usuarioElige(eleccion) {
-        let resultado = this.ganador(eleccion, this.pcJuega());
+        let pcEleccion = this.pcJuega();
+        console.log(`${this.jugador.nombre} ha seleccionado: ${eleccion}`);
+        console.log(`La computadora ha seleccionado: ${pcEleccion}`);
+        this.mostrarJugadas(eleccion, pcEleccion);
+
+        let resultado = this.ganador(eleccion, pcEleccion);
         if (resultado === "¡Ganaste!") {
             this.jugador.incrementarVictorias();
         } else if (resultado === "¡Perdiste!") {
             this.pc.incrementarVictorias();
         }
-        console.log("Victorias del jugador: " + this.jugador.victorias);
-        console.log("Victorias de la computadora: " + this.pc.victorias);
+        this.actualizarVictorias();
 
         if (this.jugador.victorias === 5) {
-            console.log("¡Felicidades! Has ganado la partida.");
+            this.mostrarMensajeFinal(`¡Felicidades, ${this.jugador.nombre}! Has ganado la partida.`);
+            this.finalizarJuego();
         } else if (this.pc.victorias === 5) {
-            console.log("La computadora ha ganado la partida. ¡Mejor suerte la próxima vez!");
+            this.mostrarMensajeFinal("La computadora ha ganado la partida. ¡Mejor suerte la próxima vez!");
+            this.finalizarJuego();
         }
     }
 
@@ -113,5 +146,31 @@ class Juego {
             console.log("¡Perdiste!");
             return "¡Perdiste!";
         }
+    }
+
+    mostrarJugadas(jugador, pc) {
+        document.getElementById('jugadas').classList.remove('hidden');
+        document.getElementById('jugadaJugador').innerText = `El jugador ha elegido: ${jugador} `;
+        document.getElementById('jugadaComputadora').innerText = ` La computadora ha elegido: ${pc}`;
+    }
+
+    actualizarVictorias() {
+        document.getElementById('resultados').style.display = 'flex';
+        document.getElementById('victoriasJugador').innerText = `Victorias de ${this.jugador.nombre}: ${this.jugador.victorias}`;
+        document.getElementById('victoriasComputadora').innerText = ` Victorias de la computadora: ${this.pc.victorias}`;
+    }
+
+    mostrarMensajeFinal(mensaje) {
+        const mensajeFinal = document.createElement('div');
+        mensajeFinal.className = 'mensajeFinal';
+        mensajeFinal.innerHTML = `<h1>${mensaje}</h1>`;
+        const jugarBtn2 = document.getElementById('jugarBtn2');
+        document.body.insertBefore(mensajeFinal, jugarBtn2);
+    }
+
+    finalizarJuego() {
+        document.querySelector('.imagenes').classList.add('escondido');
+        document.getElementById('jugarBtn2').classList.remove('escondido');
+        document.getElementById('jugarBtn2').style.display = 'block';
     }
 }
